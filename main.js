075f6d57d61fbe3fc01ef7aa83bfdf736244e4fc
rapipang <108124869+rapipang@users.noreply.github.com>
2023-05-30 03:20:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -537,13 +537,15 @@ Particle.prototype.update = function (stage) {
 
 function init() {
   window.scrollTo(0, 0);
-  alert('ALERT! This page will auto-play music and sound FX');
   var loader = new OBJLoader();
   loader.load("/media/graphs.obj",
 
     function (twitr) {
       window.stage = new Stage(twitr);
     });
+  
+alert('ALERT! This page will auto-play music and sound FX');
 
 }
 window.onload = init;
+
